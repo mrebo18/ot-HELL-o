d7cae3d40bc3b67fb63156cde9c2083b17f1686c
Malia Rebollo <mrebollo@caltech.edu>
2018-03-05 17:15:12 -0800

    Same board

--- board.cpp
+++ board.cpp
@@ -183,6 +183,7 @@ int Board::heuristic(Move *m, Side side) {
     return heuristic;
 }
 
+
 /*
  * Sets the board state given an 8x8 char array where 'w' indicates a white
  * piece and 'b' indicates a black piece. Mainly for testing purposes.
