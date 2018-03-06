6aa38abccf21d2adeb5fc5cdfa479160a9d97811
Malia Rebollo <mrebollo@caltech.edu>
2018-03-05 17:18:53 -0800

    Adding heuristic to Move

--- common.hpp
+++ common.hpp
@@ -8,7 +8,7 @@ enum Side {
 class Move {
    
 public:
-    int x, y;
+    int x, y, h;
     Move(int x, int y) {
         this->x = x;
         this->y = y;        
@@ -17,9 +17,11 @@ public:
 
     int getX() { return x; }
     int getY() { return y; }
+    int getHeur() {return h; }
 
     void setX(int x) { this->x = x; }
     void setY(int y) { this->y = y; }
+    void setHeur(int h) { this->h = h; }
 };
 
 #endif
